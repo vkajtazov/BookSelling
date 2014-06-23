da678addbfc191f44739ad7fee8c3c167581cd3b
vkajtazov <kajtazov.viktor@gmail.com>
2014-06-23 13:09:40 +0200

    Added Login Page

--- BookSelling/BookSelling/MainWindow.xaml.cs
+++ BookSelling/BookSelling/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BookSelling
 {
@@ -24,14 +25,101 @@ namespace BookSelling
     {
 
         private static string connectionString = BookSelling.Properties.Settings.Default.connectionString;
+        private string[] MenuList;
+        private static int Height = 50;
+        private static int Width = 150;
+        private DispatcherTimer HeightTimer;
+        private DispatcherTimer WidthTimer;
 
         public MainWindow()
         {
             InitializeComponent();
-
-
+            loadHeightTimer();
+            loadWidthTimer();
             // testing connection with database
-            lst.ItemsSource = DatabaseConnectionFile.getDataTable("Users").DefaultView;
+        //    lst.ItemsSource = DatabaseConnectionFile.getDataTable("Users").DefaultView;
         }
+
+        public void loadHeightTimer() {
+            HeightTimer = new DispatcherTimer();
+            HeightTimer.Tick += new EventHandler(HeightTimer_Tick);
+            HeightTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+        }
+
+        public void loadWidthTimer()
+        {
+            WidthTimer = new DispatcherTimer();
+            WidthTimer.Tick += new EventHandler(WidthTimer_Tick);
+            WidthTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+        }
+
+        private void HeightTimer_Tick(object sender, EventArgs e)
+        {       
+
+                
+             CommandManager.InvalidateRequerySuggested();
+             Application.Current.MainWindow.Height += 2;
+             if (Height <= 0) {
+                 HeightTimer.Stop();
+                 WidthTimer.Start();
+             }
+             Height -= 1;
+        }
+
+        private void WidthTimer_Tick(object sender, EventArgs e)
+        {
+
+
+            CommandManager.InvalidateRequerySuggested();
+            Application.Current.MainWindow.Width += 2;
+            if (Width <= 0)
+            {
+                WidthTimer.Stop();
+                BookPresentation.Visibility = Visibility.Visible;
+            }
+            Width -= 1;
+        }
+
+
+
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (checkRequiredFields()) {
+                LoginView.Visibility = Visibility.Collapsed;
+                HeightTimer.Start();
+                LoggedAs.Content = "Logged as: " + UsernameTxt.Text;
+            }
+
+        }
+
+        private bool checkRequiredFields() {
+            bool t = true;
+
+            if (UsernameTxt.Text == "")
+            {
+                usernameValidator.Visibility = Visibility.Visible;
+                t = false;
+            }
+            if (PasswordTxt.Text == "")
+            {
+                passswordValidator.Visibility = Visibility.Visible;
+                t = false;
+            }
+
+            if (UsernameTxt.Text != "")
+            {
+                usernameValidator.Visibility = Visibility.Hidden;
+            }
+            if (PasswordTxt.Text != "")
+            {
+                passswordValidator.Visibility = Visibility.Hidden;
+            }
+            return t;
+        }
+
+
+
     }
 }
