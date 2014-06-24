2641f671f7650590190d4eac8ab35d4003963420
vkajtazov <kajtazov.viktor@gmail.com>
2014-06-24 17:07:16 +0200

    Template edited

--- BookSelling/BookSelling/GenreConverter.cs
+++ BookSelling/BookSelling/GenreConverter.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Data;
 
 namespace BookSelling
 {
-    public class CheckPromotionsConverter:IValueConverter
+    class GenreConverter:IValueConverter
     {
-
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             int val = (int)value;
-            if (val > 0) return TextDecorations.Strikethrough;
+            if (val == 1) { return "Drama"; }
+            else if (val == 2) { return "Romance"; }
+            else if (val == 3) { return "Satire"; }
+            else if (val == 4) { return "Tragedy"; }
+            else if (val == 5) { return "Comedy"; }
+            else if (val == 6) { return "Tragicomedy"; }
             return null;
         }
 
